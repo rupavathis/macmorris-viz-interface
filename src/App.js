183a494b011f9db42b84ba2e30684f3f9e4ddfd3
rupavathis <rupa5094it@gmail.com>
2022-07-29 11:03:33 +0100

    Fetch and set api proxy

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import './App.css';
-import './NetworkUI/InfoBar/Info.css'
+import "./App.css";
+import "./NetworkUI/InfoBar/Info.css";
 // import Network from './NetworkUI/home'
 // import MapUI from './MapUI/home'
-import SearchUI from './SearchUI/home'
-// import ProfileUI from './ProfilePageUI/home'
-import React from 'react';
+// import SearchUI from "./SearchUI/home";
+import ProfileUI from './ProfilePageUI/home'
+import React from "react";
 
 function App() {
-
   // const [pageId, setPageId] = useState(0);
-  
 
   return (
     <div className="App">
-        {/* <button id = '1' onClick={getPageId}> Map </button>      */}
-        {<SearchUI />}
-        
+      <form action="../../post" method="post" className="form">
+        <button type="submit">Connected?</button>
+      </form>
+      {<ProfileUI />}
     </div>
   );
 }
